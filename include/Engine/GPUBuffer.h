5749253f91850b6718bb26ec1cf646e9dc77b043
tsukikage22 <151722910+tsukikage22@users.noreply.github.com>
2025-10-23 21:44:19 +0900

    ConstantBufferの修正
    バッファの作成を独自にせず，GPUBufferに委譲するように設計を変更した

--- include/Engine/GPUBuffer.h
+++ include/Engine/GPUBuffer.h
@@ -3,6 +3,7 @@
 #include <d3d12.h>
 
 #include <cstring>
+#include <optional>
 
 #include "Engine/ComPtr.h"
 
@@ -11,7 +12,7 @@ public:
     GPUBuffer() : m_Size(0) {}
     ~GPUBuffer() { Term(); }
 
-    /// @brief VBやIBなどの静的バッファを生成する
+    /// @brief VBやIBなどの静的バッファを作成する
     /// @param pDevice
     /// @param pCmdList
     /// @param size
@@ -22,15 +23,28 @@ public:
         ID3D12GraphicsCommandList* pCmdList, size_t size, const void* pInitData,
         D3D12_RESOURCE_STATES finalState);
 
+    /// @brief 変換行列などの更新が必要なバッファを作成する
+    /// @param pDevice
+    /// @param size 必要があればアラインメントして渡す
+    /// @return
+    bool CreateDynamic(ID3D12Device* pDevice, size_t size);
+
     void DiscardUpload();
 
     void Term();
 
+    //==============================================================
+    // アクセサ
+    //==============================================================
+    ID3D12Resource* GetResource() const { return m_pRes.Get(); }
     D3D12_GPU_VIRTUAL_ADDRESS GetGPUVirtualAddress() const;
+    void* GetMappedPtr() const { return m_pMappedData; }
 
 private:
     engine::ComPtr<ID3D12Resource> m_pRes;     // バッファリソース
     engine::ComPtr<ID3D12Resource> m_pUpload;  // アップロード用バッファリソース
     size_t m_Size;
     D3D12_RESOURCE_STATES m_State = D3D12_RESOURCE_STATE_COMMON;
+    bool m_IsMapped               = false;
+    void* m_pMappedData           = nullptr;
 };
--- src/Engine/ConstantBuffer.cpp
+++ src/Engine/ConstantBuffer.cpp
@@ -4,7 +4,7 @@
 
 // コンストラクタ
 ConstantBuffer::ConstantBuffer()
-    : m_pCB(nullptr), m_pPool(nullptr), m_pMappedData(nullptr) {}
+    : m_pPool(nullptr), m_pMappedData(nullptr), m_index(0) {}
 
 // デストラクタ
 ConstantBuffer::~ConstantBuffer() { Term(); }
@@ -26,49 +26,25 @@ bool ConstantBuffer::Init(
     const size_t align = D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT;
     UINT64 sizeAligned = (size + align - 1) & ~(align - 1);
 
-    // 定数バッファの生成
-    D3D12_HEAP_PROPERTIES prop = {};
-    prop.Type                  = D3D12_HEAP_TYPE_UPLOAD;
-    prop.CPUPageProperty       = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
-    prop.MemoryPoolPreference  = D3D12_MEMORY_POOL_UNKNOWN;
-    prop.VisibleNodeMask       = 1;
-    prop.CreationNodeMask      = 1;
-
-    D3D12_RESOURCE_DESC desc = {};
-    desc.Dimension           = D3D12_RESOURCE_DIMENSION_BUFFER;
-    desc.Alignment           = 0;
-    desc.Width               = sizeAligned;
-    desc.Height              = 1;
-    desc.DepthOrArraySize    = 1;
-    desc.MipLevels           = 1;
-    desc.Format              = DXGI_FORMAT_UNKNOWN;
-    desc.SampleDesc.Count    = 1;
-    desc.SampleDesc.Quality  = 0;
-    desc.Layout              = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-    desc.Flags               = D3D12_RESOURCE_FLAG_NONE;
-
-    // リソースの生成
-    auto hr = pDevice->CreateCommittedResource(&prop, D3D12_HEAP_FLAG_NONE,
-        &desc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr,
-        IID_PPV_ARGS(m_pCB.GetAddressOf()));
-    if (FAILED(hr)) {
+    // バッファの作成とメモリマッピング
+    if (!m_buffer.CreateDynamic(pDevice, sizeAligned)) {
         m_pPool->Free(m_index);
         return false;
     }
 
-    // メモリマッピング
-    hr = m_pCB->Map(0, nullptr, &m_pMappedData);
-    if (FAILED(hr)) {
+    m_pMappedData = m_buffer.GetMappedPtr();
+    if (m_pMappedData == nullptr) {
+        m_buffer.Term();
         m_pPool->Free(m_index);
-        m_pCB.Reset();
         return false;
     }
 
-    m_GPUAddress = m_pCB->GetGPUVirtualAddress();
+    m_GPUAddress = m_buffer.GetGPUVirtualAddress();
 
+    // CBVの作成
     D3D12_CONSTANT_BUFFER_VIEW_DESC CBVdesc = {};
     CBVdesc.BufferLocation                  = m_GPUAddress;
-    CBVdesc.SizeInBytes = static_cast<UINT>(m_pCB->GetDesc().Width);
+    CBVdesc.SizeInBytes                     = static_cast<UINT>(sizeAligned);
 
     pDevice->CreateConstantBufferView(&CBVdesc, pPool->GetCPUHandle(m_index));
 
@@ -78,10 +54,7 @@ bool ConstantBuffer::Init(
 // 終了処理
 void ConstantBuffer::Term() {
     // メモリマッピングの解除
-    if (m_pCB != nullptr) {
-        m_pCB->Unmap(0, nullptr);
-        m_pCB.Reset();
-    }
+    m_buffer.Term();
 
     // ビューの破棄
     if (m_pPool != nullptr) {
@@ -95,7 +68,12 @@ void ConstantBuffer::Term() {
 
 // 定数バッファの更新
 void ConstantBuffer::Update(const void* pData, size_t size) {
-    if (m_pMappedData != nullptr && pData != nullptr && size > 0) {
-        memcpy(m_pMappedData, pData, size);
+    if (m_pMappedData == nullptr || pData == nullptr || size == 0) {
+        return;
     }
+    if (size > m_size) {
+        assert(false && "buffer overflow");
+        return;
+    }
+    memcpy(m_pMappedData, pData, size);
 }
