3e6437306c63518417897d5e43160e5bf8a9ca75
tsukikage22 <151722910+tsukikage22@users.noreply.github.com>
2025-12-10 16:05:09 +0900

    AppにおけるWindowクラスとApplicationクラスの実装
    エントリーポイントとウィンドウの初期化，メインループを実装した

---
+++ src/App/Application.cpp
@@ -0,0 +1,70 @@
+#include "App/Application.h"
+
+Application::Application()  = default;
+Application::~Application() = default;
+
+int Application::Run() {
+    if (!Init()) {
+        return -1;
+    }
+
+    MainLoop();
+
+    Term();
+
+    return 0;
+}
+
+// 初期化（ウィンドウの作成，D3D・ゲームロジックの初期化）
+bool Application::Init() {
+    // ウィンドウの作成
+    const int windowWidth  = 1280;
+    const int windowHeight = 720;
+
+    if (!m_Window.Create(windowWidth, windowHeight, L"DirectX Game")) {
+        return false;
+    }
+
+    // TODO: エンジンの初期化
+    m_Engine.Initialize();
+
+    return true;
+}
+
+void Application::Term() {
+    // ウィンドウの破棄
+    m_Window.Destroy();
+
+    // エンジンの終了処理
+    m_Engine.Shutdown();
+}
+
+void Application::MainLoop() {
+    // 実行中フラグを立てる
+    m_isRunning = true;
+
+    // メインループ
+    while (m_isRunning) {
+        // 1.メッセージポンプ
+        // OSからのメッセージを処理する
+        if (!m_Window.ProcessMessages()) {
+            m_isRunning = false;
+            break;
+        }
+
+        // 2. 描画処理
+        // フレーム開始
+        m_Engine.BeginFrame();
+
+        // 定数バッファの書き込み
+        m_Engine.Update();
+
+        // 描画コマンド発行
+        m_Engine.Render();
+
+        // フレーム終了
+        m_Engine.EndFrame();
+
+        // 画面表示
+        m_Engine.Present();
+    }
---
+++ src/App/Window.cpp
@@ -0,0 +1,97 @@
+#include "App/Window.h"
+
+namespace /* anonymous */ {
+/// @brief ウィンドウプロシージャ
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    auto instance =
+        reinterpret_cast<Window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+
+    switch (msg) {
+        case WM_CREATE: {
+            auto pCreateStruct = reinterpret_cast<LPCREATESTRUCT>(lParam);
+            SetWindowLongPtr(hWnd, GWLP_USERDATA,
+                reinterpret_cast<LONG_PTR>(pCreateStruct->lpCreateParams));
+        } break;
+
+        case WM_DESTROY: {
+            PostQuitMessage(0);
+        } break;
+
+        default: {
+        } break;
+    }
+
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+// ウィンドウクラス名
+const auto ClassName = TEXT("DirectXGameWindowClass");
+}  // namespace
+
+bool Window::Create(int width, int height, const wchar_t* title) {
+    // インスタンスハンドルを取得
+    m_hInst = GetModuleHandle(nullptr);
+    if (m_hInst == nullptr) {
+        return false;
+    }
+
+    // ウィンドウクラスの設定
+    WNDCLASSEX wc    = {};
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc   = WndProc;
+    wc.hIcon         = LoadIcon(m_hInst, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(m_hInst, IDC_ARROW);
+    wc.hbrBackground = GetSysColorBrush(COLOR_BACKGROUND);
+    wc.lpszMenuName  = nullptr;
+    wc.lpszClassName = ClassName;
+
+    // ウィンドウの登録
+    if (!RegisterClassEx(&wc)) {
+        return false;
+    }
+
+    // ウィンドウサイズの設定
+    RECT rc    = {};
+    rc.right   = static_cast<LONG>(width);
+    rc.bottom  = static_cast<LONG>(height);
+    auto style = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU;
+    AdjustWindowRect(&rc, style, FALSE);
+
+    // ウィンドウの作成
+    m_hWnd = CreateWindowEx(0, ClassName, TEXT("DirectX Game"), style,
+        CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top,
+        nullptr, nullptr, m_hInst, this);
+
+    if (m_hWnd == nullptr) {
+        return false;
+    }
+
+    return true;
+}
+
+void Window::Destroy() {
+    if (m_hInst != nullptr) {
+        UnregisterClass(ClassName, m_hInst);
+    }
+
+    m_hInst = nullptr;
+    m_hWnd  = nullptr;
+}
+
+bool Window::ProcessMessages() {
+    MSG msg = {};
+
+    // メッセージループ
+    // そのフレームのメッセージをすべて処理する
+    // QUITメッセージが来たらfalseを返す
+    while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE)) {
+        if (msg.message == WM_QUIT) {
+            return false;
+        }
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return true;
+}
