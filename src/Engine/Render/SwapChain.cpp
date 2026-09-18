75ca299d7fcfc817eaa9fe6554908a80a9ba8a42
tsukikage22 <151722910+tsukikage22@users.noreply.github.com>
2026-09-18 18:38:40 +0900

    DXGIのAlt+Enterによる排他フルスクリーンへの切り替えを無効化した．
    排他フルスクリーンの状態管理を実装しておらず，ボーダーレスフルスクリーンと食い違うため無効化しておく．

--- src/Engine/Render/SwapChain.cpp
+++ src/Engine/Render/SwapChain.cpp
@@ -26,6 +26,9 @@ bool SwapChain::Init(GraphicsDevice& graphicsDevice, uint32_t width, uint32_t he
         graphicsDevice.GetFactory()->CreateSwapChainForHwnd(
             graphicsDevice.GetCommandQueue().GetD3DQueue(), hWnd, &desc, nullptr, nullptr, pSwapChain.GetAddressOf()));
 
+    // Alt+Enterによるフルスクリーン切り替えを無効化
+    graphicsDevice.GetFactory()->MakeWindowAssociation(hWnd, DXGI_MWA_NO_ALT_ENTER);
+
     // IDXGISwapChain3を取得
     CHECK_HR(graphicsDevice.GetDevice(), pSwapChain.As(&m_pSwapChain));
 
